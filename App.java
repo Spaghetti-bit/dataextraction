b18c63b8eaeb21bfa3caa5ab62f4514aa96df470
zsteber <100639026+zsteber@users.noreply.github.com>
2022-10-26 11:34:45 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+package dataextractiongroup;
+
+import java.io.DataInput;
+
+
+public final class App {
+    private App() {
+    }
+
+    /**
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
